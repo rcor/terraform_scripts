a82ed62f694449baf1c7a6dea3b26336f23b458a
Rodolfo Cordero Sancho <rodolfo.cordero@corp.smtsoftware.com>
2020-05-20 22:53:33 -0300

    add metrics

--- eks/setup_cluster.sh
+++ eks/setup_cluster.sh
@@ -8,5 +8,8 @@ echo 'create a service account:alb-ingress-controller'
 kubectl apply -f ./yml/rbac-role.yaml
 echo 'create an ingress create a controller'
 kubectl apply -f ./yml/alb-ingress-controller.yaml
+echo 'install metrics'
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+kubectl get deployment metrics-server -n kube-system
 
 #kubectl edit deployment.apps/alb-ingress-controller -n kube-system
